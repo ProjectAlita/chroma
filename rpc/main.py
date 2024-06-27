e3ca78827395cb97fcaa38e8a59fa597b3e46ab1
Aspect13 <ad13box@gmail.com>
2024-06-27 17:04:46 +0400

    fixing chroma vectorstore

--- rpc/main.py
+++ rpc/main.py
@@ -19,18 +19,18 @@ class RPC:
         # result = {'connection_string'}
         return "pgvector", result
 
-    @rpc('vector_store_details')
-    def vector_store_details(self, project_id: Optional[int], integration_uid: str, datasource_version_uuid: str):
-        integration = rpc_tools.RpcMixin().rpc.call.integrations_get_by_uid(
-            integration_uid, project_id, check_all_projects=True
-        )
-        if integration is None:
-            raise ValueError(f'Integration with {integration_uid=} not found')
-        return {
-            "vectorstore": 'chroma',
-            "vectorstore_params": {
-                "collection_name": datasource_version_uuid,
-                "persist_directory": f"{integration.settings.get('path')}/{project_id}",
-                "integration_uid": integration_uid,
-            }
-        }
+    # @rpc('vector_store_details')
+    # def vector_store_details(self, project_id: Optional[int], integration_uid: str, datasource_version_uuid: str):
+    #     integration = rpc_tools.RpcMixin().rpc.call.integrations_get_by_uid(
+    #         integration_uid, project_id, check_all_projects=True
+    #     )
+    #     if integration is None:
+    #         raise ValueError(f'Integration with {integration_uid=} not found')
+    #     return {
+    #         "vectorstore": 'chroma',
+    #         "vectorstore_params": {
+    #             "collection_name": datasource_version_uuid,
+    #             "persist_directory": f"{integration.settings.get('path')}/{project_id}",
+    #             "integration_uid": integration_uid,
+    #         }
+    #     }
